b2f47df78d9ec5a2d0fbd5d90a5942ea23b3087d
mperez6109 <mperez6109@gmail.com>
2020-02-03 17:57:24 -0800

    My first commit

---
+++ precache-manifest.4fa81a1323e68d9b1d023b945652df61.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "881563d6444b370fa4ceea0c3183bb4c",
+    "url": "/static/media/J4o.881563d6.gif"
+  },
+  {
+    "revision": "591645fdea5efd658965",
+    "url": "/static/css/main.bcba8859.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a1843fef6d5c5a12d0880ed95976a345",
+    "url": "/static/media/software.a1843fef.jpg"
+  },
+  {
+    "revision": "8c5413358e0a702a3092",
+    "url": "/static/js/2.28fbbbbe.chunk.js"
+  },
+  {
+    "revision": "4a546c9832a26148ca5effa70afcf276",
+    "url": "/static/media/frontend-400x200.4a546c98.jpg"
+  },
+  {
+    "revision": "a2b7792b6dd191e408dea429f2fc3f9e",
+    "url": "/static/media/backend-400x200.a2b7792b.jpg"
+  },
+  {
+    "revision": "cb4f5aac60accd14a14803589c831ba3",
+    "url": "/static/media/ecommerce-cart-400x200.cb4f5aac.jpg"
+  },
+  {
+    "revision": "591645fdea5efd658965",
+    "url": "/static/js/main.78d938d8.chunk.js"
+  },
+  {
+    "revision": "5efd71b3c969d4207dd1672038276f91",
+    "url": "/static/media/seo-company400x221.5efd71b3.jpg"
+  },
+  {
+    "revision": "52b8b5639c4bd655fde5099ed64c4da3",
+    "url": "/static/media/ecommerce-Technologies400x250.52b8b563.jpg"
+  },
+  {
+    "revision": "6375bdcb5c790d6c1a6c9a39cb16356b",
+    "url": "/static/media/dataScience400x200.6375bdcb.jpg"
+  },
+  {
+    "revision": "6dd8817a24a63c09139b013679253002",
+    "url": "/static/media/programming-plc-languages400x200.6dd8817a.jpg"
+  },
+  {
+    "revision": "4ab3a3d8d04b6891915db1ccf814a33f",
+    "url": "/static/media/software-Companies-755x420.4ab3a3d8.jpg"
+  },
+  {
+    "revision": "8c5413358e0a702a3092",
+    "url": "/static/css/2.b61ca34d.chunk.css"
+  },
+  {
+    "revision": "e4518925fc76e6c8dd311c8320c8ecbf",
+    "url": "/index.html"
+  }
+];
